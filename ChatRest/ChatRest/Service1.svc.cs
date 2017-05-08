637e8293e9b9fdbbaa42b09cfed2350a07b9831b
Kifayat <Kifayat@LAPTOP-2TA4DMF1>
2017-05-08 13:01:44 +0200

    kifayat

--- ChatRest/ChatRest/Service1.svc.cs
+++ ChatRest/ChatRest/Service1.svc.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Channels;
 using System.ServiceModel.Web;
 using System.Text;
 
@@ -12,6 +13,17 @@ namespace ChatRest
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        
+        private const string ConnectionString =
+            "Server=tcp:domiserver2.database.windows.net,1433;Initial Catalog=ChatDB;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        private static List<Messages> Messages = new List<Messages>()
+        {
+            new Messages("hi i am kifayat"),
+            new Messages("Hi i am John")
+            
+        };
+        public IList<Messages> GetAllMessages()
+        {
+            return Messages;
+        }
     }
 }

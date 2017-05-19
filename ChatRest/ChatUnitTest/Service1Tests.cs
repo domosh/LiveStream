74392a4e7f42ade31d6d07e02d22ed0f0d148dee
shanjana <shanjana@DESKTOP-LM00NKR>
2017-05-19 11:30:51 +0200

    u8y8

---
+++ ChatRest/ChatUnitTest/Service1Tests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ChatRest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatRest.Tests
+{
+    [TestClass]
+    public class Service1Tests
+    {
+        private readonly IService1 s = new Service1();
+
+        [TestMethod]
+        public void GetAllMessagesTest()
+        {
+           List<Message> result = s.GetAllMessages();
+            Assert.AreEqual();
+        }
+
+        [TestMethod]
+        public void AddMessage()
+        {
+            string result = s.AddMessage();
+            Assert.AreEqual();
+        }
+    }
+}
